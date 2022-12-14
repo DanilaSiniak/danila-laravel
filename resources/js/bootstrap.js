6749b11654f1bd41a5820e21a114961dc8e4901b
Alexei Fedasenko <aliaksei.fedasenka@innowise-group.com>
2022-12-14 14:28:44 +0300

    Fix pusher uploading. And add laravel deb_bar

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,5 +1,4 @@
 import _ from 'lodash';
-
 window._ = _;
 
 /**
@@ -20,12 +19,13 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  */
 
 import Echo from 'laravel-echo';
+import Pusher from 'pusher-js';
 
-window.Pusher = require('pusher-js');
+window.Pusher = Pusher;
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true
 });
