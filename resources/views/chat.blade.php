f3f44e4580d9c7d4392ddda624cc1fe0884678c0
DanilaSiniak <sinyak2002@gmail.com>
2022-12-20 17:59:11 +0100

    added a better interface

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -2,27 +2,47 @@
 
 @section('content')
     <div class="row">
-        <div class="col-5 text-center">
-            место для взаимодействия с другими участниками чата
-        </div>
-        <div class="col-5 text-right">
-            <div class="justify-between border-b-2 border-gray-200">
-                    <div class="flex flex-col leading-tight">
-                        <div class="text-2xl items-center">
-                            <span class="text-gray-700">{{ auth()->user()->name }}</span>
-                        </div>
+        <div class="col-sm-4 col-lg-3">
+            <div class="card">
+                <div class="card-header"><b>Users</b></div>
+                <div class="card-body" id="user_list">
 
-                        <span class="text-lg text-gray-600">{{ auth()->user()->email }}</span>
                 </div>
             </div>
+        </div>
+        <div class="col-sm-4 col-lg-6">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col col-md-6" id="chat_header"><b>Chat</b></div>
 
-            <div id="app">
-                <div class="space-y-4 p-3 overflow-y-auto scrollbar-track-blue h-screen">
-                    <chat-messages :user="{{ auth()->user() }}"></chat-messages>
+                        <div class="space-y-4 p-3 overflow-y-auto scrollbar-track-blue h-screen">
+                            <chat-messages :user="{{ auth()->user() }}"></chat-messages>
+                        </div>
+
+                        <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2">
+                            <chat-form></chat-form>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </div>
+        <div class="col-sm-4 col-lg-3">
+            <div class="card" style="height:255px; overflow-y: scroll;">
+                <div class="card-header">
+                    <input type="text" class="form-control" placeholder="Search User..." autocomplete="off" id="search_people" onkeyup="search_user('{{ Auth::id() }}', this.value);" />
+                </div>
+                <div class="card-body">
+                    <div id="search_people_area" class="mt-3"></div>
+                </div>
+            </div>
+            <br />
+            <div class="card" style="height:255px; overflow-y: scroll;">
+                <div class="card-header"><b>Notification</b></div>
+                <div class="card-body">
+                    <ul class="list-group" id="notification_area">
 
-                <div class="border-t-2 border-gray-200 px-4 pt-4 mb-2">
-                    <chat-form></chat-form>
+                    </ul>
                 </div>
             </div>
         </div>
